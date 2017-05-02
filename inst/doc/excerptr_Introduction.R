e30f066fb9658e8b47a3a199512829cd174a9401
Andreas Dominik Cullmann <adc-r@arcor.de>
2017-05-02 05:33:44 +0000

    version 1.0.2

--- inst/doc/excerptr_Introduction.R
+++ inst/doc/excerptr_Introduction.R
@@ -6,17 +6,23 @@ cat(readLines(path), sep = "\n")
 excerptr::excerptr(file_name = path, run_pandoc = FALSE, output_path = tempdir())
 
 ## ---- comment = ""-------------------------------------------------------
-cat(readLines(file.path(tempdir(), sub("\\.R$", ".md", basename(path)))), sep = "\n")
+cat(readLines(file.path(tempdir(), sub("\\.R$", ".md", basename(path)))), 
+    sep = "\n")
 
 ## ------------------------------------------------------------------------
-excerptr::excerptr(file_name = path, pandoc_formats= "html", output_path = tempdir())
+if (length(Sys.which("pandoc") >= 0))
+    excerptr::excerptr(file_name = path, pandoc_formats= "html", 
+                       output_path = tempdir())
 
 ## ---- comment = ""-------------------------------------------------------
-cat(readLines(file.path(tempdir(), sub("\\.R$", ".html", basename(path)))), sep = "\n")
+if (length(Sys.which("pandoc") >= 0))
+    cat(readLines(file.path(tempdir(), sub("\\.R$", ".html", basename(path)))), 
+        sep = "\n")
 
 ## ---- echo = FALSE, results = "hide"-------------------------------------
-excerptr::excerptr(file_name = path, pandoc_formats= "html", 
-                   output_path = file.path(rprojroot::find_root(rprojroot::is_r_package), 
-                                           "inst", "tests", "files")
-                   )
+if (length(Sys.which("pandoc") >= 0))
+    excerptr::excerptr(file_name = path, pandoc_formats= "html", 
+                       output_path = file.path(rprojroot::find_root(rprojroot::is_r_package), 
+                                               "inst", "tests", "files")
+                       )
 
